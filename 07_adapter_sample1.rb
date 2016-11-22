c2a146c9b8dc8fd590aac79d022785acf432f000
yamakichi <y.kazuki0614n@gmail.com>
2016-11-23 00:38:56 +0900

    commit adapter

---
+++ 07_adapter_sample1.rb
@@ -0,0 +1,54 @@
+# Targetにはないインターフェイスを持つ(Adaptee)
+class OldPrinter
+  def initialize(string)
+    @string = string.dup
+  end
+
+  def show_with_paren
+    puts "(#{@string})"
+  end
+
+  def show_with_aster
+    puts "*#{@string}*"
+  end
+end
+
+# 利用者(Client)へのインターフェイス(Target)
+class Printer
+  def initialize(obj)
+    @obj = obj
+  end
+
+  def print_weak
+    @obj.print_weak
+  end
+
+  def print_strong
+    @obj.print_strong
+  end
+end
+
+
+# Targetが利用出来るインターフェイスに変換(Adapter)
+class Adapter
+  def initialize(string)
+    @old_printer = OldPrinter.new(string)
+  end
+
+  def print_weak
+    @old_printer.show_with_paren
+  end
+
+  def print_strong
+    @old_printer.show_with_aster
+  end
+end
+
+
+# ================================================
+# 利用者(Client)
+p = Printer.new(Adapter.new("Hello"))
+
+p.print_weak
+
+p.print_strong
